2f5bb6232740c442b59515803c737a47c249a3c0
surajakit <akitmescrop@gmail.com>
2025-05-06 12:31:52 +0530

    Update HF3one.py

--- HF3one.py
+++ HF3one.py
@@ -376,7 +376,7 @@ def news():
     live_video_html = """
     <h2>Live Weather and Disaster Video</h2>
     <div class="ratio ratio-16x9 mb-4">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/cVajz9Sb47Q?si=a5IWAOzuIpHb_JYi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>" title="Live Weather News" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/De5KQEDX41g?si=C4iXoGD15DWQBHJd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen" title="Live Weather News" allowfullscreen></iframe>
     </div>
     """
 
